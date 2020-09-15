ec11e5d91ccb1e47f98e6288f8e1ecbdb7416a4e
timothy.reynolds <timothy.reynolds@dsainc.com>
2020-09-15 12:12:01 -0400

    Structure of application is underway, csv data has been read into a iterable array for processing.

---
+++ app/util/file-reader.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	//"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	// Open the file
+	csvfile, err := os.Open("../data/database.csv")
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	// Parse the file
+	r := csv.NewReader(csvfile)
+
+	// Iterate through the records
+	for {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Longitude: %s Latitude %s\n", record[2], record[3])
+	}
+}
