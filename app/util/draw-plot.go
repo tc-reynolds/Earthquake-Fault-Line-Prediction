6887eacb9a7406e1bf4ef851f4af568b4c5fa2dd
timothy.reynolds <timothy.reynolds@dsainc.com>
2020-09-21 20:29:14 -0400

    Earthquake data is plotted with mapping correction to look exclusively at the mainland USA (no Alaska or territories)

--- app/util/draw-plot.go
+++ app/util/draw-plot.go
@@ -6,41 +6,83 @@ import (
     "gonum.org/v1/plot/plotutil"
     "gonum.org/v1/plot/vg"
     "fmt"
+	"strconv"
+    "log"
 )
 
-func DrawPlot() {
 
+var data [][]string
+var minX = 25
+var maxX = 50
+var minY = 75
+var maxY = 130
+
+func DrawPlot() {
+	data = LoadDatabase()
+	fmt.Println("DrawPlot entered.")
     p, err := plot.New()
-    if err != nil {
-        panic(err)
+	fmt.Println("Plot Object created")
+	if err != nil {
+        log.Fatal(err)
     }
 
     p.Title.Text = "Plotutil example"
-    p.X.Label.Text = "X"
-    p.Y.Label.Text = "Y"
+    p.X.Label.Text = "Longitude"
+    p.Y.Label.Text = "Latitude"
+	fmt.Println("Plot Axis Labels initialized")
 
-    err = plotutil.AddScatters(p,
-        "Earthquake Report", GraphPoints(LoadDatabase()))
+	err = plotutil.AddScatters(p,
+        "Earthquake Report", GraphPoints(data))
     if err != nil {
-        panic(err)
+        log.Fatal(err)
     }
-
+	fmt.Println("Earthquake Report init")
     // Save the plot to a PNG file.
-    if err := p.Save(4*vg.Inch, 4*vg.Inch, "points.png"); err != nil {
-        panic(err)
+    if err := p.Save(4*vg.Inch, 4*vg.Inch, "earthquake.png"); err != nil {
+        log.Fatal(err)
     }
 }
 
 // Graph Points returns a scatter graph of data specified in file-reader.
 func GraphPoints(data [][]string) plotter.XYs {
-    pts := make(plotter.XYs, 15)
-    for i := range pts {
-        if i == 0 {
-            pts[i].X = rand.Float64()
-        } else {
-            pts[i].X = pts[i-1].X + rand.Float64()
-        }
-        pts[i].Y = pts[i].X + 10*rand.Float64()
-    }
+	fmt.Println("GraphPoints called")
+	pts := make(plotter.XYs, len(data))
+	fmt.Println("pts initialized")
+	for i := 1; i < len(pts); i++ {
+		//fmt.Println("data[i][2]")
+		//fmt.Println(data[i][2])
+		//fmt.Println("data[i][3]")
+		//fmt.Println(data[i][3])
+		X, err := strconv.ParseFloat(data[i][2], 64)
+		if err != nil{
+			fmt.Println("float X parsing error")
+			log.Fatal(err)
+		}
+		Y, err := strconv.ParseFloat(data[i][3], 64)
+		if err != nil{
+			fmt.Println("float Y parsing error")
+			log.Fatal(err)
+		}
+		onMainland := parseData(X, Y)
+		if bool(onMainland) {
+			if i == 0 {
+				pts[i].X = X
+			} else {
+				pts[i].X = pts[i-1].X + X
+			}
+			pts[i].Y = pts[i].Y + Y
+		}
+	}
+
     return pts
+}
+func parseData(X float64, Y float64) bool{
+	//fmt.Println(int(X))
+	if int(X) > minX && int(X) < maxX{
+		if int(Y) > minY && int(Y) < maxY{
+			//fmt.Println(X, Y)
+			return true
+		}
+	}
+	return false
 }
--- app/util/file-reader.go
+++ app/util/file-reader.go
@@ -11,16 +11,22 @@ import (
 
 func LoadDatabase() [][]string{
 	// Open the file
+	fmt.Println("Loading database...")
 	csvFile, err := os.Open("./data/database.csv")
+	fmt.Println("Loaded...")
 	if err != nil {
-		log.Fatalln("Couldn't open the csv file", err)
+		log.Fatal("Couldn't open the csv file", err)
 	}
-
 	// Parse the file
 	r := csv.NewReader(csvFile)
+	fmt.Println("New Reader init...")
+
 	records, err := r.ReadAll()
-	fmt.Printf("records[1][2]")
-	fmt.Printf(records[1][2])
+	fmt.Println("Array created...")
+	fmt.Println("records[1][2]")
+	fmt.Println(records[0][2])
+	fmt.Println(records[0][3])
+
 	return records
 }
 
